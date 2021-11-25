69bb10f221a35c595e44a9c74203fb0ea3b74212
Paul Whittington <pwhittington@ricelake.com>
2021-11-24 19:31:52 -0700

    Harmonize SQL data with database schema

--- database-files/data.sql
+++ database-files/data.sql
@@ -1,18 +1,40 @@
+INSERT INTO airport 
+    (airport_code, description) 
+VALUES 
+    ("DEN", "Denver International Airport"),
+    ("LAX", "Los Angeles International Airport"), 
+    ("MCO", "Orlando International Airport"),
+    ("ORD", "Chicago O'Hare International Airport");
 
-INSERT INTO equipment (number_of_seats, description, tail_number) VALUES (234, "Airbus a354",3456), (245, "Boeing 747",276), (267, "Daron Jetblue",158);
+
+INSERT INTO customer 
+  (first_name, last_name, phone)
+VALUES 
+    ('Hektor', 'Attaway', '755.223.5969'),
+    ('Businge', 'Foster', '399.377.3854'),
+    ('Torbjørg', 'Maynard', '256.399.4665');
 
 
-INSERT INTO airport (airport_code, description) VALUES (123, "Los Angeles LAX"), (156, "Orlando International Airport (MCO)"),
- (134, "Chicago International Airport ORD"), (187, "Denver International Airport (DEN)");
+
+INSERT INTO equipment 
+    (description, number_of_seats, tail_number) 
+VALUES 
+    ("Airbus a354", 234, "N183DN"), 
+    ("Boeing 747", 276, "N832MH"), 
+    ("Daron Jetblue", 158 , "N919DU");
  
  
-INSERT INTO flight (departure_date, arrival_date, equipment_id, departure_airport_id, arrival_airport_id, description) VALUES("2021-01-23", "2021-01-23", 2, 3 , 1, "American Airline"),
-("2021-06-20", "2021-06-20", 1, 2 , 3, "American Airline"),("2021-01-23", "2021-01-23", 2, 3 , 4, "American Airline");
+INSERT INTO flight 
+    (arrival_airport_id, arrival_date_time, departure_airport_id, departure_date_time, description, equipment_id) 
+VALUES
+    (1, "2021-11-25T05:25:00Z", 2, "2021-11-25T02:55:00Z", "American Airlines", 1),
+    (1, "2021-11-25T06:25:00Z", 3, "2021-11-25T03:55:00Z", "Delta Airlines", 2),
+    (1, "2021-11-25T07:25:00Z", 4, "2021-11-25T04:55:00Z", "United Airlines", 3),
 
 
-INSERT INTO customer VALUES ('ATTAWAY_HECKTOR', 'Hektor', 'Attaway', '755.223.5969'),
-('MAYNARD_TORBJORG', 'Torbjørg', 'Maynard', '256.399.4665'),
-('FOSTER_BUSINGE', 'Businge', 'Foster', '399.377.3854');
-
-
-INSERT INTO reservation (customer_id, flight_id) VALUES ("ATTAWAY_HECKTOR", 1),('MAYNARD_TORBJORG', 3),("FOSTER_BUSINGE", 2);
+INSERT INTO reservation 
+    (customer_id, flight_id) 
+VALUES 
+    (1, 1),
+    (2, 3),
+    (3, 2);
